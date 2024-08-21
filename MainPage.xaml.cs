e05054f0685c07c48c79fa91654887adfcafd3bc
lhzltbl <lhizel.0415@gmail.com>
2024-08-21 13:58:09 +0800

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Module01Exercise01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SavedChanges(object sender, EventArgs e)
+        {
+            await DisplayAlert("Your profile has been updated.", "Thank you for submitting", "Ok");
+        }
+    }
+
+}
